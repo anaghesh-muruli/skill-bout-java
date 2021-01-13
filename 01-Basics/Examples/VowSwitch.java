387dd493eb5bda2176588976d5c718ecaa163698
anaghesh-muruli <anagheshm@gmail.com>
2021-01-13 21:10:08 +0530

    day-12

---
+++ 01-Basics/Examples/VowSwitch.java
@@ -0,0 +1,21 @@
+class VowSwitch
+{
+	public static void main(String args[])
+	{	
+		char givenIp = 'b';
+		switch(givenIp)
+		{
+			case 'a' : System.out.println("Given char is Vowel");
+					   break;
+			case 'e' : System.out.println("Given char is Vowel");
+					   break;
+			case 'i' : System.out.println("Given char is Vowel");
+					   break;
+			case 'o' : System.out.println("Given char is Vowel");
+					   break;
+			case 'u' : System.out.println("Given char is Vowel");
+					   break;
+			default :  System.out.println("Given char is Consonent");
+		}
+	}
+}
